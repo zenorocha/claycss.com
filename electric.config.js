36c9113a17dae670f7c9322f80e7e02eb3876afd
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-04-06 11:29:11 -0700

    Update vendor dest paths

--- electric.config.js
+++ electric.config.js
@@ -3,14 +3,30 @@
 const lexicon = require('lexicon-ux');
 const path = require('path');
 
+var lexiconJSPath = path.join(lexicon.srcDir, 'js');
+
 module.exports = {
 	metalComponents: ['electric-quartz-components'],
 	sassOptions: {
 		includePaths: ['node_modules', lexicon.includePaths]
 	},
 	vendorSrc: [
-		path.join(lexicon.buildDir, 'images', 'icons', '*'),
-		path.join(lexicon.srcDir, 'js', 'svg4everybody.js'),
-		'node_modules/source-sans-pro/**'
+		{
+			dest: 'dist/vendor/source-sans-pro',
+			src: 'node_modules/source-sans-pro/**'
+		},
+		{
+			dest: 'dist/vendor/lexicon',
+			src: path.join(lexicon.buildDir, 'images', 'icons', '*')
+		},
+		{
+			src: [
+				path.join(lexiconJSPath, 'svg4everybody.js'),
+				path.join(lexiconJSPath, 'bootstrap.js')
+			]
+		},
+		{
+			src: path.join(lexiconJSPath, 'svg4everybody.js')
+		}
 	]
 };
